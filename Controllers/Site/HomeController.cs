e3ce1c34885ad6efd08dd58336d402d0a49b384c
nguyentrieu2210 <nguyenvantrieu092003@gmail.com>
2023-10-22 10:35:32 +0700

    completed homepage by Nguyen Trieu

--- Controllers/Site/HomeController.cs
+++ Controllers/Site/HomeController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using vphone.Models;
 
 namespace vphone.Controllers.Site
 {
     public class HomeController : Controller
     {
+        private QLQuanDTContext db;
+        public HomeController(QLQuanDTContext db)
+        {
+            this.db = db;
+        }
+
         public IActionResult Index()
         {
-            return View("~/Views/Site/Home/Index.cshtml");
+             ViewBag.products_featured = db.Products
+                                            .OrderByDescending(p => p.CreatedAt)
+                                            .Where(p => p.IsFeatured == true && p.IsStock == true)
+                                            .Take(6).
+                                            ToList();
+            ViewBag.products = db.Products.OrderByDescending(p => p.CreatedAt)
+                                          .Where(p => p.IsStock == true)
+                                          .Take(6)
+                                          .ToList();
+
+            return View("~/Views/Site/Home/Index.cshtml", ViewBag);
         }
     }
 }
