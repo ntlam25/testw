8304b3e73064591fc06f77aa7702a7cb82864bdd
ntlam25 <lam2k3d@gmail.com>
2023-10-23 11:37:49 +0700

    Update category paging

--- Controllers/Site/CategoryController.cs
+++ Controllers/Site/CategoryController.cs
@@ -19,7 +19,7 @@ namespace vphone.Controllers.Site
         public IActionResult Index(string slug, int? page)
         {
             if (page == null) page = 1;
-            int pageSize = 1;
+            int pageSize = 6;
             int pageNumber = (page ?? 1);
             var products = db.Products.Where(l => l.Cat.Slug == slug).Include(l => l.Cat).ToList();
             return View("~/Views/Site/Category/Index.cshtml", products.ToPagedList(pageNumber, pageSize));
